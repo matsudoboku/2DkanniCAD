6cb35ba0fd99d1364b81f9cf768fc1807fb3e4af
matsudoboku <muramatsu.matsukawa@gmail.com>
2025-06-09 23:59:10 +0900

    Create dxf.ts

---
+++ client/src/features/client/src/client/src/client/src/client/src/client/src/utils/dxf.ts
@@ -0,0 +1,9 @@
+// Placeholder utilities for importing and exporting DXF files.
+// Implement conversions between your canvas elements and DXF entities here.
+export function exportDXF() {
+  // TODO: use dxf-writer to build a DXF file
+}
+
+export function importDXF(file: File) {
+  // TODO: use dxf-parser to read a DXF file
+}
