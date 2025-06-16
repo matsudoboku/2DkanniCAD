14a62e166665e1b7af080368671e94970ca43c56
matsudoboku <muramatsu.matsukawa@gmail.com>
2025-06-16 22:55:22 +0900

    Delete client directory

--- client/src/features/client/src/client/src/client/src/client/src/client/src/utils/dxf.ts
+++
@@ -1,9 +0,0 @@
-// Placeholder utilities for importing and exporting DXF files.
-// Implement conversions between your canvas elements and DXF entities here.
-export function exportDXF() {
-  // TODO: use dxf-writer to build a DXF file
-}
-
-export function importDXF(file: File) {
-  // TODO: use dxf-parser to read a DXF file
-}
